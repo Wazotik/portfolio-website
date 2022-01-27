93b4f3d30ba2f9aa63eb709c0b0afc953b307d81
Wahaj <wahajhaider32@gmail.com>
2022-01-26 22:41:32 -0500

    created project container for 1 projects inside project section

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ const IndexPage = () => {
             <img
               src={require("../images/wahaj-face-pic.jpg").default}
               alt=""
-              className="max-w-[300px] rounded-2xl m-0"
+              className="max-w-[300px] rounded-2xl m-0 shadow-2xl"
             />
           </div>
           <div>
@@ -45,9 +45,12 @@ const IndexPage = () => {
               <p className="text-white text-lg tracking-wide mb-2">
                 I've recently taken an interest in <b>web development</b> and
                 have had a lot of fun building web applications using
-                technologies/frameworks such as <b>JavaScript</b>, <b>HTML</b>, <b>CSS</b>, <b>React</b>, <b>Express.js</b>, <b>SQL</b>, etc. I'm also proficient in other languages
-                such as <b>Java</b> and <b>Python</b>. I plan to further extend my knowledge by exploring more frontend technologies/frameworks
-                and diving deeper into the backend side of things.
+                technologies/frameworks such as <b>JavaScript</b>, <b>HTML</b>,{" "}
+                <b>CSS</b>, <b>React</b>, <b>Express.js</b>, <b>SQL</b>, etc.
+                I'm also proficient in other languages such as <b>Java</b> and{" "}
+                <b>Python</b>. I plan to further extend my knowledge by
+                exploring more frontend technologies/frameworks and diving
+                deeper into the backend side of things.
               </p>
             </div>
           </div>
@@ -57,9 +60,39 @@ const IndexPage = () => {
         id="projects"
         className="bg-white flex justify-center items-center"
       >
-        <div className="w-8/12 flex flex-row justify-center items-center pt-20 pb-20 pr-40 pl-40 gap-20">
+        <div className="w-11/12 flex flex-col justify-center items-center pt-20 pb-20 pr-40 pl-40 gap-20">
           <div className="text-5xl tracking-wider mb-5 text-center">
             Projects
+          </div>
+          <div className="flex justify-around items-center w-[100%] flex-wrap">
+            <div className="border-solid border-4 border-[#2b2b2b] w-64 h-[26rem] rounded-xl mr-10 ml-10 flex flex-col p-3 hover:bg-[#2b2b2b] hover:text-white transition-colors duration-500">
+              <div className="flex flex-col flex-0 justify-start items-center">
+                <img
+                  className="shadow-sm mb-3 rounded-t-md"
+                  src={require("../images/heart-watch-screenshot.png").default}
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col flex-1">
+                <h4 className="mb-0 text-lg">Heart-Watch</h4>
+                <p className="m-0 text-sm leading-5">
+                  A website providing an overview for Heart-Watch, a mobile
+                  application providing a modern and simple solution for
+                  cardiovascular disease prevention and protection.
+						<div className="italic mt-2">
+							This project was submitted to DeltaHacks 8.
+						</div>
+                </p>
+              </div>
+              <div className="flex flex-row flex-1 justify-center items-end">
+                <a className="flex flex-1 justify-center" target="_blank" href="https://heartwatch.tech/">
+                  Live Demo
+                </a>
+                <a className="flex flex-1 justify-center" target="_blank" href="https://heartwatch.tech/">
+						 GitHub
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </section>
